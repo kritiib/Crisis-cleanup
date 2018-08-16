1c262d873d12a3adba0e3e5dc2023536cc508763
Wonder Woman <kritii.bhattarai@gmail.com>
2018-08-17 02:31:23 +0545

    Added choose level and roadmap card section

--- scripts/custom/scripts.js
+++ scripts/custom/scripts.js
@@ -6,6 +6,33 @@
 
     /* ========================= Helper functions based on vanilla javascript ============================ */
 
+    /*
+	** Add heloper class on DOM element on window resize
+	*/
+
+    activateResizeHandler();
+
+    function activateResizeHandler() {
+        var resizeClass = 'resize-active',
+            flag, timer;
+
+        var removeClassHandler = function() {
+            flag = false;
+            d.documentElement.classList.remove(resizeClass);
+
+        };
+
+        var resizeHandler = function() {
+            if(!flag) {
+                flag = true;
+                d.documentElement.classList.add(resizeClass);
+            }
+            clearTimeout(timer);
+            timer = setTimeout(removeClassHandler, 10);
+        };
+        $(w).on('resize orientationchange', resizeHandler);
+    };
+
     /**
      * @param el
      * @param _nodeClass
@@ -126,5 +153,48 @@
 
         });
     }
+
+    /**
+     * Detecting elements in each last row to add helper classes to the columns of last row
+     */
+
+    getLastRowCols($('.js-row'), $('.js-last-col'));
+
+    function getLastRowCols(container, elem) {
+        container.each(function() {
+            var $self = $(this);
+            var colInRow = 0;
+            //var $container = null;
+            $self.find(elem).each(function() {
+                if($(this).prev().length > 0) {
+                    if($(this).position().top != $(this).prev().position().top) return false;
+                    colInRow++;
+                }
+                else {
+                    colInRow++;
+                }
+                var colInLastRow = $self.find(elem).length % colInRow;
+                if(colInLastRow == 0) colInLastRow = colInRow;
+                $self.find(elem).removeClass('is-last-col')
+                $self.find(elem).slice(-colInLastRow).addClass('is-last-col')
+            });
+        });
+    }
+
+    /**
+     * Recalculate calculations on window resize
+     */
+    var winTimer = null;
+    var winTimerDelay = 10;
+
+    $(w).on('resize', function() {
+        if(winTimer != null) clearTimeout(winTimer);
+        winTimer = setTimeout(function() {
+
+            // == Reinit getLastRowCols on window resize
+            getLastRowCols($('.js-row'), $('.js-last-col'));
+
+        }, winTimerDelay);
+    });
 
 })(window, document, jQuery);
